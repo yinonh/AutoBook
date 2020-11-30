4cb96f69e114d2a9c017f448df429908d9234a91
yinonh <yinon.h21@gmail.com>
2020-11-30 19:57:31 +0200

    default picture for books

--- book_catalog/models.py
+++ book_catalog/models.py
@@ -1,17 +1,15 @@
 from django.db import models
 
 class Book (models.Model):
-    book_id = models.IntegerField()
     name = models.CharField(max_length=50)
     author_name = models.CharField(max_length=50)
     summary = models.TextField(max_length=500)
-    image = models.ImageField(upload_to='book_catalog/images')
-    #user_queue =
+    image = models.ImageField(upload_to='book_catalog/images', default='book_catalog/images/defult.png')
+    #user_queue = NOT RELLEVANT YET
     available = models.BooleanField(default=True)
-    #location =
+    #location =NOT RELLEVANT YET
     study_book = models.BooleanField(default=False)
     key_words =models.CharField(max_length=100)
-    #categories = models.Choices(names=Op)
     length = models.IntegerField(blank=True)
     reader_name = models.CharField(max_length=50, blank=True)
     GENRE_CHOICES = (
