bec7879fcfe10af7fdbe88e01335bd88e7b95197
yahalomit vaizman <yahalomit28@gmail.com>
2020-12-28 19:46:02 +0200

    design

--- homepage/models.py
+++ homepage/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class HomePage(models.Model):
-    title=models.CharField(max_length=100)
-    description=models.CharField(max_length=250)
-    image=models.ImageField(upload_to='homepage/images/',default='book_catalog/images/defult.png')
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=400)
+    link = models.URLField(blank=True)
+    image = models.ImageField(upload_to='homepage/images/',default='book_catalog/images/defult.png')
