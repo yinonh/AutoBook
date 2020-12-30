f778399372f682679e317268786e80a939c4b899
shaywe@ac.sce.ac.il <6415528a@gmail.com>
2020-12-30 18:03:28 +0200

    added favourite books to adult

--- authentication/models.py
+++ authentication/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import MinLengthValidator
-
+from book_catalog.models import Book
 
 
 
@@ -20,8 +20,8 @@ class Adult(models.Model):
     user=models.OneToOneField(User, on_delete=models.SET_NULL,null=True,blank=True)
     ID_Number=models.CharField(max_length=10,default=None)
     type = models.CharField(max_length=50,default='Adult')
-    Is_Banned = models.BooleanField(default=False,validators=[MinLengthValidator(10)])
+    Is_Banned = models.BooleanField(default=False)
     Security_Check = models.BooleanField(default=False)
-    
+    FavouriteBooks=models.ManyToManyField(Book,null=True,blank=True)
     def __str__(self):
         return self.user.username
