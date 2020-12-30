f31054cbbce3cd6ca305031de2f1e824e7aa2e06
shaywe@ac.sce.ac.il <6415528a@gmail.com>
2020-12-30 17:47:28 +0200

    added security check

--- authentication/models.py
+++ authentication/models.py
@@ -12,7 +12,7 @@ class Student(models.Model):
     type=models.CharField(max_length=50,default='Student')
     Forum_Banned=models.BooleanField(default=False)
     Phone_Number = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
-
+    Security_Check = models.BooleanField(default=False)
     def __str__(self):
         return self.user.username
 
@@ -20,7 +20,8 @@ class Adult(models.Model):
     user=models.OneToOneField(User, on_delete=models.SET_NULL,null=True,blank=True)
     ID_Number=models.CharField(max_length=10,default=None)
     type = models.CharField(max_length=50,default='Adult')
-    Is_Banned = models.BooleanField(default=False)
-
+    Is_Banned = models.BooleanField(default=False,validators=[MinLengthValidator(10)])
+    Security_Check = models.BooleanField(default=False)
+    
     def __str__(self):
         return self.user.username
